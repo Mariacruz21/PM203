09cfbda49727e6ed2c603e4fcb9c5b4cdcd7bc27
Maria Cruz Camargo Araujo <123047616@upq.edu.mx>
2026-06-16 20:25:06 -0600

    Practica8 segunda parte

--- Practica5/miapp2/App.js
+++ Practica5/miapp2/App.js
@@ -1,23 +1,32 @@
-/*ZONA 1: Importaciones de componentes */
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import { Saludo } from './Components/Saludo';
-import { Saludo2 } from './Components/Saludo2';
+import { StyleSheet, View } from 'react-native';
 import { Perfil } from './Components/Perfil';
 
-
-/*ZONA 2: Componente Principal */
 export default function App() {
   return (
     <View style={styles.container}>
 
-      <Perfil nombre="Maria Cruz Camargo Araujo" carrera="Ingeniería en Sistemas Computacionales" materia="Programación Móvil" cuatrimestre="9°" />
-
+      <Perfil
+        estiloExt={styles.tarjetaRoja}
+        nombre="Maria Cruz Camargo Araujo"
+        carrera="Ingeniería en Sistemas Computacionales"
+        materia="Programación Móvil"
+        cuatrimestre="9°"
+      />
 
       <Perfil
+        estiloExt={styles.tarjetaVerde}
         nombre="MariCruz"
         carrera="ICS"
         materia="PM"
+        cuatrimestre="9°"
+      />
+
+      <Perfil
+        estiloExt={styles.tarjetaRoja}
+        nombre="Maria Cruz Camargo Araujo"
+        carrera="Ingeniería en Sistemas Computacionales"
+        materia="Programación Móvil"
         cuatrimestre="9°"
       />
 
@@ -26,12 +35,21 @@ export default function App() {
   );
 }
 
-/*ZONA 3: Estilos y Posicionamiento */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
+    flexDirection: 'row',
+  },
+
+
+  tarjetaRoja: {
+    backgroundColor: 'red',
+  },
+
+  tarjetaVerde: {
+    backgroundColor: 'green',
   },
 });
